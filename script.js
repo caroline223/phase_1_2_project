7efa5c5c69232b644ae8d26cec62e4c71db5f5a8
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-05-01 19:30:25 -0400

    Creating the generateRecipe function

--- script.js
+++ script.js
@@ -8,3 +8,36 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelector("#forward").addEventListener("click", nextPage)
     document.querySelector("#back").addEventListener("click", previousPage)
 })
+
+function getAllRecipes(){
+    fetch(pageURL)
+    .then(response => response.json())
+    .then(recipeArr => {
+        recipeArr.forEach(generateRecipe)
+    })
+}
+
+function generateRecipe(recipe) {
+    let recipeDiv = document.createElement('div')
+    document.querySelector('#recipe-container').appendChild(recipeDiv)
+
+
+    //generating the recipe's name
+    recipeName = document.createElement('h3')
+    recipeName.innerText = recipe.name 
+    recipeDiv.appendChild(recipeName)
+
+
+    //generating the recipe's ingredients
+    recipeIngredients = document.createElement('p')
+    recipeIngredients.innerText = recipe.ingredients 
+    recipeDiv.appendChild(recipeIngredients)
+
+    //generating the recipe's description
+
+    recipeDes = document.createElement('p')
+    recipeDes.innerText = recipe.description 
+    recipeDiv.appendChild(recipeDes)
+
+    //generating the recipe's image (if applicable)
+}
