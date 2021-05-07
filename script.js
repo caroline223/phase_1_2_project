07f2f8aa2914e873a0f4704c220fa20d2d847797
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-05-07 00:54:01 -0400

    updating the page functionality

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ let pageURL = `https://api.punkapi.com/v2/beers?page=${pageNum}&per_page=20`;
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    getAllDrinks();
     document.querySelector("#random-drink-button").addEventListener("click", randomDrink)
-    document.querySelector("#forward").addEventListener("click", nextPage)
-    document.querySelector("#back").addEventListener("click", previousPage)
+    document.querySelector("#featured-drink-button").addEventListener("click", getAllDrinks)
 })
 
 function getAllDrinks(){
@@ -16,10 +14,17 @@ function getAllDrinks(){
         console.log(drinkArr)
         drinkArr.forEach(generateDrink)
     })
-    .catch(error => {
-        alert("File Not Available");
-        console.log(error.message);
-    })
+
+    
+    const backButton = document.createElement('button')
+        backButton.innerHTML="Previous"
+        document.querySelector('#drink-button').appendChild(backButton)
+    const forwardButton = document.createElement('button')
+        forwardButton.innerHTML="Next" 
+        document.querySelector('#drink-button').appendChild(forwardButton)
+   
+        backButton.addEventListener("click", previousPage)
+        forwardButton.addEventListener("click", nextPage)
 }
 
 function generateDrink(drink) {
@@ -67,12 +72,54 @@ function generateDrink(drink) {
 function randomDrink(){ 
     fetch('https://api.punkapi.com/v2/beers/random')
     .then(response => response.json())
-    .then(drinkArr => {
-        console.log(drinkArr)
+    .then(randomArr => {
+        console.log(randomArr)
         document.querySelector("#random-drink").innerHTML=""
-        drinkArr.map(generateDrink)
+        randomArr.map(generateRandomDrink)
     })
 } 
+
+function generateRandomDrink(random) {
+    let randomDiv = document.createElement('div')
+    document.querySelector('#random-drink').appendChild(randomDiv)
+
+    //drink's name
+    randomName = document.createElement('h3')
+    randomName.innerText = random.name 
+    randomDiv.appendChild(randomName)
+
+    //drink's image
+    randomImg = document.createElement('img')
+    randomImg.src = random.image_url ? random.image_url : './default_image.png'
+    randomImg.width = 300
+    randomImg.height = 500
+    randomDiv.appendChild(randomImg)
+
+    //drink's tag line
+    randomTag = document.createElement('p')
+    randomTag.innerText = "Tag Line: " + random.tagline
+    randomDiv.appendChild(randomTag)
+
+    //date the drink was brewed
+    randomDate = document.createElement('p')
+    randomDate.innerText =  "Date Brewed: " + random.first_brewed
+    randomDiv.appendChild(randomDate)
+
+    //drink's description
+    randomDes = document.createElement('p')
+    randomDes.innerText = "Description: " + random.description 
+    randomDiv.appendChild(randomDes)
+
+    //drink's food pairing
+    randomFood = document.createElement('p')
+    randomFood.innerText =  "Food Pairing: " + random.food_pairing
+    randomDiv.appendChild(randomFood)
+
+    //drink's brewer's tips
+    randomBrew = document.createElement('p')
+    randomBrew.innerText =  "Brewer's Tips: " + random.brewers_tips
+    randomDiv.appendChild(randomBrew)
+}
 
 /*function newDrink(event) {
     event.preventDefault()
@@ -117,11 +164,12 @@ function previousPage(e) {
 }
 
 function nextPage(e) {
-    fetch(`https://api.punkapi.com/v2/beers?page=${pageNum+=1}&per_page=20`)
-    .then(response => response.json())
-    .then(drinkArr => {
-        //place an if-else conditional here
-        document.querySelector("#drink-container").innerHTML=""
-        drinkArr.forEach(generateDrink)
-    })
+        fetch(`https://api.punkapi.com/v2/beers?page=${pageNum+=1}&per_page=20`)
+        .then(response => response.json())
+        .then(drinkArr => {
+                document.querySelector("#drink-container").innerHTML=""
+                console.log(drinkArr)
+                drinkArr.forEach(generateDrink)  
+    })               
 }
+
